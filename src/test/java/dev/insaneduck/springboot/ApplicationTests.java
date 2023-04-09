6bd8180e137420f2bd57fe19a88701d3a7005a5d
satya <39309626+InsaneDuck@users.noreply.github.com>
2023-04-09 17:35:30 +0100

    updated

--- src/test/java/dev/insaneduck/springboot/ApplicationTests.java
+++ src/test/java/dev/insaneduck/springboot/ApplicationTests.java
@@ -1,15 +1,14 @@
 package dev.insaneduck.springboot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests
 {
 
-    @Test
-    void contextLoads()
-    {
-    }
+//    @Test
+//    void contextLoads()
+//    {
+//    }
 
 }
